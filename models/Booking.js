6cd70f8cc57183865467b1d1ebdc396ac6675d67
Ernedo <45565036+Ernedo@users.noreply.github.com>
2024-05-19 18:04:05 +0200

    Update Booking.js

--- models/Booking.js
+++ models/Booking.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String },
   phone: { type: String, required: true },
   date: { type: String, required: true },
   time: { type: String, required: true },
